f763d8449d2dfc46f3d2ea8d6619f645c831c468
Alex <phithounsavanh@gmail.com>
2023-06-10 17:33:00 -0400

    put error middlware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const productRoutes = require('./routes/productRoute');
+const errorMiddleware = require('./middleware/errorMiddleware');
 const app = express()
 
 const PORT = process.env.PORT || 5000
@@ -10,17 +11,20 @@ const MONGO_URL = process.env.MONGO_URL
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+
 //routes
 
-// app.use("/api/products", productRoutes);
+app.use("/api/products", productRoutes);
 
 app.get('/', (req, res) => {
-    res.send('Hello NODE API')
+    throw new Error('this is error');
 })
 
 app.get('/blog', (req, res) => {
     res.send('Hello Blog, My name is Devtamin')
 });
+
+app.use(errorMiddleware);
 
 
 mongoose.set("strictQuery", false)
